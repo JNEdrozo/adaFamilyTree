e9b015a81de152b5824b7792b13a9e9ea5b1ceed
Jan Edrozo <jedrozo@gmail.com>
2018-01-09 21:45:43 -0800

    Added index html template for adaTree and temporarily listing first_name of all profiles

--- adaTree/urls.py
+++ adaTree/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+from django.conf.urls import url
+
 app_name = 'adaTree'
+
 urlpatterns = [
     path('', views.index, name="index"),
 ]
--- adaTree/views.py
+++ adaTree/views.py
@@ -4,10 +4,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from django.shortcuts import render
 from django.http import Http404
 
 from django.shortcuts import get_object_or_404, render
 
+from .models import OptInProfile
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the adaTree index.")
+    #return HttpResponse("Hello, world. You're at the adaTree index.")
+    template = loader.get_template('adaTree/index.html')
+    profiles = OptInProfile.objects.all()
+    context = {
+        'profiles': profiles
+    }
+
+    return render(request, 'adaTree/index.html', context)
