b8627f5db2cdd38beb8fe54fba684b76ad573822
Jan Edrozo <jedrozo@gmail.com>
2018-01-17 14:00:55 -0800

    Added a log-in template and urls for django user authentication

--- adaTree/urls.py
+++ adaTree/urls.py
@@ -4,8 +4,13 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
 app_name = 'adaTree'
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     path('', views.index, name="index"),
     path('adaTree/', views.adaTree, name="adaTree"),
     #path('', views.IndexView.as_view(), name='index'), #using generic ListView
