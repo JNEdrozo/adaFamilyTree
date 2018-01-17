aade4f95c42cc23f32741479b22e32dabc6f71a5
Jan Edrozo <jedrozo@gmail.com>
2018-01-16 16:08:10 -0800

    Added list_display for CohortAdmin and OptInProfileAdmin so rows display readable info other than object and id number

--- adaTree/admin.py
+++ adaTree/admin.py
@@ -8,11 +8,12 @@ from .models import OptInProfile
 #admin.site.register(Profile)
 
 class OptInProfileAdmin(admin.ModelAdmin):
-    search_fields = ('first_name', 'last_name', 'id', )
+    search_fields = ('first_name', 'last_name', 'id', 'cohort_served',)
+    list_display = ('first_name', 'last_name', 'id', 'cohort_served',)
 
 class CohortAdmin(admin.ModelAdmin):
     search_fields = ('cohort_name',)
-
+    list_display = ('cohort_name',)
 admin.site.register(Cohort, CohortAdmin)
 
 admin.site.register(OptInProfile, OptInProfileAdmin)
