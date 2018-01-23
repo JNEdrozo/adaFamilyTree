f537ba0815e2d7e68a0e0dcbb9af3ac6fb271180
Jan Edrozo <jedrozo@gmail.com>
2018-01-23 02:04:33 -0800

    Adjusted profile info styling for index.html(Ada Students) and sidebar buttons

--- adaTree/static/js/slide.js
+++ adaTree/static/js/slide.js
@@ -2,14 +2,23 @@
 $(document).ready(function () {
   $('#sidebarCollapse').on('click', function() {
     $('#sidebar').toggleClass('active');
-    $(this).toggleClass('active');
+    $(this).toggleClass('active')
+    // $('.head-nav').toggleClass('head-nav-slide');
+
   });
 
+  // $('.member-header').on('click', '.member-close-btn', function() {
+  //   $('.member').toggleClass('hidden');
+  // });
+
   $('.member-close-btn').on('click', function() {
-    $('#member').toggleClass('hidden');
+    $('.member').toggleClass('hidden');
   });
 
   $('#night-option').click(function() {
     $('body').toggleClass('night-mode');
+    // $('.head-nav').toggleClass('night-mode-header');
+
+
   });
 });
