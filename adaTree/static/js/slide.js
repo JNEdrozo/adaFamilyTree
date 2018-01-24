8b87a7260c82f1ec77fb734d17a14e04a7fb9800
Jan Edrozo <jedrozo@gmail.com>
2018-01-24 15:28:09 -0800

    Adjusted sidebar styling after night-mode button and header was finalized; Also added grow class to header-logo and menu button to grow on hover

--- adaTree/static/js/slide.js
+++ adaTree/static/js/slide.js
@@ -3,13 +3,8 @@ $(document).ready(function () {
   $('#sidebarCollapse').on('click', function() {
     $('#sidebar').toggleClass('active');
     $(this).toggleClass('active')
-    // $('.head-nav').toggleClass('head-nav-slide');
 
   });
-
-  // $('.member-header').on('click', '.member-close-btn', function() {
-  //   $('.member').toggleClass('hidden');
-  // });
 
   $('.member-close-btn').on('click', function() {
     $('.member').toggleClass('hidden');
@@ -17,7 +12,8 @@ $(document).ready(function () {
 
   $('#night-option').click(function() {
     $('body').toggleClass('night-mode');
-    // $('.head-nav').toggleClass('night-mode-header');
+    $('.head-nav').toggleClass('night-mode-header');
+    // $('#button#sidebarCollapse.navbar-btn').toggleClass('sidebarCollapse-btn-night-mode');
 
   });
 
