add5b75adfdbc85e515ee97b118cc4ed1f0d20ea
Jan Edrozo <jedrozo@gmail.com>
2018-01-24 22:45:44 -0800

    Added studentsLabel url path and view to connect to studentsLabel.html

--- adaTree/views.py
+++ adaTree/views.py
@@ -188,6 +188,79 @@ def students(request):
 
 
 @login_required
+def studentsLabel(request):
+
+    template = loader.get_template('adaTree/studentsLabel.html')
+
+    profiles = OptInProfile.objects.all()
+    cohorts = Cohort.objects.all()
+
+    nodes = [{"id": "Ada Developers Academy", "full_name": "Ada Developers Academy", "type": 'program',}]
+    #nodes = []
+    links = []
+
+    for profile in profiles:
+        # Node data for D3
+        nodes.append({
+            "id": profile.pk,
+            "program": profile.program,
+            "full_name": profile.first_name + ' ' + profile.last_name,
+            "relation": profile.ada_relation,
+            "pronouns": profile.pronouns,
+            "cohort": profile.cohort_served,
+            "internship": profile.internship_placement,
+            "linkedin": profile.linkedin,
+            "capstone": profile.capstone_info,
+            "type": 'student',
+        })
+        # Link data for D3
+        links.append({
+            "source": profile.pk,
+            "target": profile.cohort_served,
+            # "target": {"id": profile.cohort_served},
+            "value": 2,
+        })
+
+    for cohort in cohorts:
+        nodes.append({
+            "id": cohort.cohort_name,
+            "full_name": cohort.cohort_name,
+            "cohort": cohort.cohort_name,
+            "type": 'cohort',
+        })
+        links.append({
+            "source": cohort.cohort_name,
+            "target": "Ada Developers Academy",
+            # "target": {"id": profile.cohort_served},
+            "value": 3,
+        })
+
+    # serialized_nodes = serializers.serialize('json', nodes)
+    # serialized_links = serializers.serialize('json', links)
+
+    data = {
+     # "nodes": serialized_nodes,
+     "nodes": nodes,
+     "links": links
+    }
+
+    # profiles = OptInProfile.objects.only("ada_relation", "capstone_info", "cohort_served", "first_name", "internship_placement", "last_name", "linkedin", "program", "pronouns")
+    # serialized_context = serializers.serialize('json', json)
+    # serialized_links = serializers.serialize('json', links)
+
+    # context = {
+    #     'nodes': serialized_nodes,
+    #     'links': links
+    # }
+
+    context = {
+        'json': json.dumps(data)
+    }
+
+    return render(request, 'adaTree/studentsLabel.html', context)
+
+
+@login_required
 def adaTree(request):
 
     template = loader.get_template('adaTree/adaTree.html')
